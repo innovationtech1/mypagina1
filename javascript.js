4864a7bc9619e96669962363990c6739fe12045b
Hector788 <hectordehoyos053@gmail.com>
2020-03-09 06:27:24 -0500

    Esta es una pagina de prueba se puede utilizar para publicar un sitio web

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function apretar(){
+  confirm ('Este es un mensaje de javascript pulsa aceptar ');
+}
+
+function opcion1(){
+    confirm('Cierra la ventana de exporacion en VSC')
+};
+function opcion2(){
+    confirm(' Selecciona parte del codigo en archivos de VSC')
+};
+function opcion3(){
+    confirm('Funciona para poner el cursor en dos o mas lineas codigo al mismos tiempo efectivo para escribir planas ')
+};
+function opcion4(){
+    confirm('Duplica el codigo seleccionado ')
+};
+function opcion5(){
+    confirm('Ayuda a mover el codigo seleccionado ')
+};
+
+
